47865ac23806fc4ab60e97e0287a1160707a62a3
RazaAnsari <raza0908@gmail.com>
2023-07-28 05:24:54 +0500

    Update map

--- main.dart
+++ main.dart
@@ -43,17 +43,49 @@ void main() {
   print(IDs);
 // Usinf PutIfAbsent will should update Hasan as Hammad at 2, but it didn't happen
 //------------------------------
-var words ={
-  1:"sky", 2:"Dream;", 3:"Ham", 4:"Key",5:"rock",6:"Ocean"
-};
-words.remove(1);
-print(words);
+  var words = {
+    1: "sky",
+    2: "Dream;",
+    3: "Ham",
+    4: "Key",
+    5: "rock",
+    6: "Ocean"
+  };
+  words.remove(1);
+  print(words);
 // words.removeWhere((2, Dream) => value.startWith('k'));
 // print(words);
-words.clear();
-print(words);
+  words.clear();
+  print(words);
 //------------------------\
 //---------------------
+//_______MERGE METHOD____
+  var f1 = {1: "Apple", 2: "Mango"};
+  var f2 = {3: "Orange"};
+  var f3 = {4: "Banana"};
+  var fruity = {} //Empty cell k sath cascasde operator add kradia ""..addAll"
+    ..addAll(f1)
+    ..addAll(f2)
+    ..addAll(f3);
+  print(fruity);
+  print("Merge Method");
+  var fruits = {...f1, ...f3}; // three dots are "spread operator | ..."
+  print(fruits);
 
+// ----------CONTAINS KEY & CONTAINS VALUE ------------
+
+  var myMap = {1: "Apple", 2: "Banana", 3: "Cherry"};
+  print(myMap.containsKey(1)); // True
+  print(myMap.containsKey(4)); // False
+
+  print(myMap.containsValue("Apple")); // True
+  print(myMap.containsValue("Orange")); // False
+//---------------------------------
+  //------- ForEach Element -------------
+  var veg = {1: "Apple", 2: "Pine", 3: "Orange"};
+  veg.forEach((key, value) {
+    print("$key $value");
+  });
+
+  
 }
- 

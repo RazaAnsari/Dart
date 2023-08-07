5f85916b48a0d4e2aed194b3f339f2256e6d3d0a
RazaAnsari <raza0908@gmail.com>
2023-08-08 00:59:02 +0500

    Practice

---
+++ practice_project.dart
@@ -0,0 +1,72 @@
+import 'dart:io';
+
+// void main() {
+// //   for (var i = 1; i < 5; i++) {
+// //     for (var j = 1; j <= i; j++) {
+// //       stdout.write("*");
+// //     }
+// //     print("");
+// //   }
+// //   // *
+// //   // **
+// //   // ***
+// //   // ****
+// //   print("--------------------------");
+// //   var rows = 5;
+// //   var a = " ";
+// //   for (var i = 1; i < rows; i++) {
+// //     stdout.write("${a * (rows - i)}");
+// //     for (var j = 1; j <= i; j++) {
+// //       stdout.write("*");
+// //     }
+// //     print("");
+// //   }
+// // //     *
+// // //    **
+// // //   ***
+// // //  ****
+// //   print("--------------------------");
+// //   var row = 5;
+// //   var b = " ";
+// //   for (var i = 1; i < row; i++) {
+// //     stdout.write("${b * (row - i)}");
+// //     for (var j = 1; j <= i; j++) {
+// //       stdout.write("* ");
+// //     }
+// //     print("");
+// //   }
+// // //    *
+// // //    * *
+// // //   * * *
+// // //  * * * *
+// //   print("--------------------");
+// //  var row = 5;
+// //   var b = " ";
+// //   for (var i = 1; i < row; i++) {
+// //     stdout.write("${b * (row - i)}");
+// //     for (var j = 1; j <= i; j++) {
+// //       stdout.write("${i} ");
+// //     }
+// //     print("");
+// //   }
+// // //     1
+// // //    2 2
+// // //   3 3 3
+// // //  4 4 4 4
+// //   print("--------------------");
+//   var row = 5;
+//   var b = " ";
+//   var counter = 1;
+//   for (var i = 1; i < row; i++) {
+//     stdout.write("${b * (row - i)}");
+//     for (var j = 1; j <= i; j++) {
+//       stdout.write("${counter++} ");
+//     }
+//     print("");
+//   }
+//     1 
+//    2 3 
+//   4 5 6 
+//  7 8 9 10 
+// }
+
